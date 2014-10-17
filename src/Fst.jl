af387369f1efa414b6cc50054abef652aa470db7
Oliver Adams <oliver.adams@gmail.com>
2014-10-17 17:50:32 +0100

    added exports to Fst. Moved testing code into test/runtests.jl

--- src/Fst.jl
+++ src/Fst.jl
@@ -2,6 +2,9 @@ module Fst
 
 using Lumberjack
 
+export Wfst, wfst2dot, create_pdf, add_arc, add_initial_state, add_final_state,
+        compose, states_with_no_in_edges, topological_sort
+
 # The weighted FST type.
 type Wfst
     states::Set
@@ -138,9 +141,6 @@ function compose(a::Wfst, b::Wfst)
            final_states, transitions, initial_weights, final_weights)
 end
 
-# Add a compose operator in the same style as pywfst
-(>>) = compose
-
 # Add a state to the final states list
 function add_final_state(wfst::Wfst, state::String)
     @assert state in wfst.states
@@ -189,40 +189,4 @@ function states_with_no_in_edges(wfst::Wfst)
     states_with_in_edges = Set([rule[2] for rule in wfst.transitions])
     return setdiff(wfst.states, states_with_in_edges)
 end
-
-a = Wfst()
-add_arc(a, "0", "1", "a", "b", 0.1)
-add_arc(a, "1", "0", "a", "b", 0.2)
-add_arc(a, "1", "2", "b", "b", 0.3)
-add_arc(a, "1", "3", "b", "b", 0.4)
-add_arc(a, "2", "3", "a", "b", 0.5)
-add_arc(a, "3", "3", "a", "a", 0.6)
-add_initial_state(a, "0")
-add_final_state(a, "3")
-a.final_weights["3"] = 0.7
-create_pdf(a, "a.pdf")
-
-
-b = Wfst()
-add_arc(b, "0", "1", "b", "b", 0.1)
-#add_arc(b, "1", "1", "b", "a", 0.2)
-add_arc(b, "1", "2", "a", "b", 0.3)
-add_arc(b, "1", "3", "a", "b", 0.4)
-add_arc(b, "2", "3", "b", "a", 0.5)
-add_initial_state(b, "0")
-add_final_state(b, "3")
-b.final_weights["3"] = 0.6
-create_pdf(b, "b.pdf")
-
-c = compose(a, b)
-create_pdf(c, "c.pdf")
-
-println(states_with_no_in_edges(a))
-println(states_with_no_in_edges(b))
-println(states_with_no_in_edges(c))
-
-#println(topological_sort(a))
-println(topological_sort(b))
-println(topological_sort(c))
-
 end
