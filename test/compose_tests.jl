a4504070d392528eb63c261defc48ba752b56296
Oliver Adams <oliver.adams@gmail.com>
2014-10-19 11:51:06 +0100

    added the start of some epsilon transition tests in compose_tests

--- test/compose_tests.jl
+++ test/compose_tests.jl
@@ -28,7 +28,6 @@ create_pdf(c, "c.pdf")
 =#
 
 # Tests for composition involving <epsilon> transitions
-
 t1 = Wfst()
 add_arc!(t1, 0, 1, "a", "a", 1.0)
 add_arc!(t1, 1, 2, "b", "<epsilon>", 1.0)
@@ -37,3 +36,14 @@ add_arc!(t1, 3, 4, "d", "d", 1.0)
 add_initial_state!(t1, 0, 1.0)
 add_final_state!(t1, 4, 1.0)
 create_pdf(t1, "t1.pdf")
+
+t2 = Wfst()
+add_arc!(t2, 0, 1, "a", "d", 1.0)
+add_arc!(t2, 1, 2, "<epsilon>", "e", 1.0)
+add_arc!(t2, 2, 3, "d", "a", 1.0)
+add_initial_state!(t2, 0, 1.0)
+add_final_state!(t2, 3, 1.0)
+create_pdf(t2, "t2.pdf")
+
+c = compose(t1, t2)
+create_pdf(c, "c.pdf")
