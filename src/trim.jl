b31841404ac8e11955315a11ca7fd0dcead5a0ed
Oliver Adams <oliver.adams@gmail.com>
2014-10-22 12:23:50 +0100

    added coaccessible and trim functions

--- src/trim.jl
+++ src/trim.jl
@@ -1,7 +1,7 @@
-export accessible
+export accessible, coaccessible, trim
 
 function accessible(wfst::Wfst)
-    stack = wfst.initial_states
+    stack = copy(wfst.initial_states)
     discovered_states = Any[]
     discovered_arcs = Arc[]
     # Classic DFS
@@ -18,8 +18,39 @@ function accessible(wfst::Wfst)
         end
     end
 
+    println(wfst)
+    new = deepcopy(wfst)
+    new.states = Set(discovered_states)
+    new.arcs = Set(discovered_arcs)
+    println(new)
+    return new
+end
+
+function coaccessible(wfst::Wfst)
+    stack = copy(wfst.final_states)
+    discovered_states = Any[]
+    discovered_arcs = Arc[]
+    # Classic DFS, just reversing the direction we follow arcs
+    while length(stack) > 0
+        state = pop!(stack)
+        if !(state in discovered_states)
+            push!(discovered_states, state)
+            for arc in wfst.arcs
+                # Note the reversal of arc.to and arc.from
+                if arc.to == state
+                    push!(stack, arc.from)
+                    push!(discovered_arcs, arc)
+                end
+            end
+        end
+    end
+
     new = deepcopy(wfst)
     new.states = Set(discovered_states)
     new.arcs = Set(discovered_arcs)
     return new
+end
+
+function trim(wfst::Wfst)
+    return coacccessible(accessible(wfst))
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using Fst
 using Base.Test
 
 include("compose_tests.jl")
+include("trim_tests.jl")
 
 #=
 println(Fst.states_with_no_in_arcs(a))
