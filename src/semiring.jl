bf12007d82eb4992c5bc572b2c01c51377d79da9
Oliver Adams <oliver.adams@gmail.com>
2014-10-29 15:30:55 +1100

    added the Tropical semiring

--- src/semiring.jl
+++ src/semiring.jl
@@ -1,4 +1,4 @@
-export Semiring, Probability_semiring
+export Semiring, Probability_semiring, Tropical_semiring
 
 # A semiring type that you can create. Currently the onus is on the user to
 # make sure the semiring makes sense.
@@ -10,3 +10,4 @@ immutable Semiring
 end
 
 Probability_semiring = Semiring(+, *, 0, 1)
+Tropical_semiring = Semiring(min, +, Inf, 0)
